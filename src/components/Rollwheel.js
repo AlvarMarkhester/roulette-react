d993bf528d10b6507a6de30c2cf6d4919de3affd
Alvar Markhester <alvarprivat@hotmail.com>
2022-06-15 16:14:07 +0200

    Animation has been implemeted. Not full functionality yet, but WIP

--- src/components/Rollwheel.js
+++ src/components/Rollwheel.js
@@ -22,33 +22,24 @@ const INITIAL_LAYOUT = [
   [COLORS.black, 9],
   [COLORS.red, 7],
   [COLORS.black, 8],
-  [COLORS.red, 1],
-  [COLORS.black, 14],
-  [COLORS.red, 2],
-  [COLORS.black, 13],
-  [COLORS.red, 3],
-  [COLORS.black, 12],
-  [COLORS.red, 4],
-  [COLORS.green, 0],
-  [COLORS.black, 11],
-  [COLORS.red, 5],
-  [COLORS.black, 10],
-  [COLORS.red, 6],
-  [COLORS.black, 9],
-  [COLORS.red, 7],
-  [COLORS.black, 8],
 ];
 
 function Rollwheel() {
   const [numberLayout, setNumberLayout] = useState(INITIAL_LAYOUT);
   return (
-    <div className="roll-container">
-      <div className="roll-wrapper">
-        {numberLayout.map((value) => {
-          return <div style={{ background: value[0] }}>{value[1]}</div>;
-        })}
+    <>
+      <button>Roll</button>
+      <div className="roll-container">
+        <div className="roll-wrapper animate">
+          {numberLayout.map((value) => {
+            return <div style={{ background: value[0] }}>{value[1]}</div>;
+          })}
+          {numberLayout.map((value) => {
+            return <div style={{ background: value[0] }}>{value[1]}</div>;
+          })}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
